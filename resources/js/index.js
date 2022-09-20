12b58422adffb0a0fb450ca4e0f4908c6977ae39
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-09-19 22:06:17 -0400

    Update index.js
    Syntax error on serverURL constant.

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,7 +3,7 @@ const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
-const serverURL = 'https://it3049-c-chat-application.herokuapp.com/messages';
+const serverURL = `https://it3049-c-chat-application.herokuapp.com/messages`;
 
 //Initial call
 updateMessages()
