a74945bda553ac428f81f4a998300b903898fdf3
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-09-19 21:34:21 -0400

    Update index.html ; styles.css ; index.js
    Added functions and page layout.

---
+++ resources/js/index.js
@@ -0,0 +1,88 @@
+//Referenced elements
+const nameInput = document.getElementById("my-name-input");
+const myMessage = document.getElementById("my-message");
+const sendButton = document.getElementById("send-button");
+const chatBox = document.getElementById("chat");
+const serverURL = 'https://it3049-c-chat-application.herokuapp.com/messages';
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+
+//Calling updateMessages every 10 seconds to populate new messages
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+//Update Messages
+async function updateMessages() {
+  //Fetch
+  const messages = await fetchMessages();
+
+  //Loop over
+  let formattedMessages = "";
+  messages.forEach(message => {
+    formattedMessages += formatMessage(message, nameInput.value);
+  });
+
+  //Add to the chatbox
+  chatBox.innerHTML = formattedMessages;
+}
+
+//API requests to server
+function fetchMessages() {
+  return fetch(serverURL)
+    .then(response => response.json())
+}
+
+//Formatting message
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = '${time.getHours()}:${time.getMinutes()}';
+
+  if (myNameInput === message.sender) {
+    return `
+    <div class="mine messages">
+        <div class="message">
+            ${message.text}
+        </div>
+        <div class="sender-info">
+            ${formattedTime}
+        </div>
+    </div>
+    `
+  } else {
+    return `
+    <div class="yours messages">
+      <div class="message">
+        ${message.text}
+      </div>
+      <div class="sender-info">
+        ${message.sender} ${formattedTime}
+      </div>
+    </div>
+    `
+  }
+}
+
+//Send Messages
+function sendMessages(username, text) {
+  const newMessage = {
+    sender: username,
+    text: text,
+    timestamp: new Date()
+  }
+
+  fetch (serverURL, {
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newMessage)
+  });
+}
+
+//EventListener for sendButton
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender,message);
+  myMessage.value = "";
+})
